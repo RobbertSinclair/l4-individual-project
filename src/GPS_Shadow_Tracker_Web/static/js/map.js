a10f32cd9ee7bac9302baefe9fa2218dee367e6e
robbert.sinclair <2432710S@student.gla.ac.uk>
2022-10-08 23:41:14 +0100

    Added a time filter

--- src/GPS_Shadow_Tracker_Web/static/js/map.js
+++ src/GPS_Shadow_Tracker_Web/static/js/map.js
@@ -1,9 +1,23 @@
 const map = L.map("map").setView([55.8724, -4.29], 14);
-
+const timeSelect = document.getElementById("timeSelect");
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attritution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>`
 }).addTo(map);
+
+let markerLayer = L.featureGroup();
+
+
+timeSelect.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const start = document.getElementById("start");
+    const end = document.getElementById("end");
+
+    console.log(start.value);
+    console.log(end.value);
+    const filterUrl = `/locations_time/${start.value}/${end.value}`;
+    getLocations(filterUrl);
+})
 
 function calculateColour(accuracy) {
     if (accuracy < 20) {
@@ -17,18 +31,26 @@ function calculateColour(accuracy) {
     }
 } 
 
-fetch("/all_locations")
-.then(res => res.json())
-.then(body => {
-    const locations = body.locations;
-    locations.forEach(location => {
-        let circle = L.circle([location.latitude, location.longitude], {
-            color: calculateColour(location.accuracy),
-            fillColor: calculateColour(location.accuracy),
-            fillOpacity: 0.5,
-            radius: location.accuracy
-        }).addTo(map);
-        circle.bindPopup(`Accuracy: ${location.accuracy}<br>Time: ${location.time}`)
-        console.log(`Added a circle at ${location.latitude} ${location.longitude}`)
-    })
-})
+
+function getLocations(url) {
+    fetch(url)
+    .then(res => res.json())
+    .then(body => {
+        map.removeLayer(markerLayer);
+        markerLayer = L.featureGroup(); 
+        const locations = body.locations;
+        locations.forEach(location => {
+            let circle = L.circle([location.latitude, location.longitude], {
+                color: calculateColour(location.accuracy),
+                fillColor: calculateColour(location.accuracy),
+                fillOpacity: 0.5,
+                radius: location.accuracy
+            }).addTo(markerLayer);
+            circle.bindPopup(`Accuracy: ${location.accuracy}<br>Time: ${location.time}`)
+            console.log(`Added a circle at ${location.latitude} ${location.longitude}`)
+        })
+        map.addLayer(markerLayer);
+    });
+}
+
+getLocations("/all_locations");
