d68e337ad1312824948f32433f03d96de17ce8a3
robbert.sinclair <2432710S@student.gla.ac.uk>
2022-10-08 17:09:29 +0100

    Updated the map

--- src/GPS_Shadow_Tracker_Web/static/js/map.js
+++ src/GPS_Shadow_Tracker_Web/static/js/map.js
@@ -1,15 +1,8 @@
-const map = L.map("map").setView([51.505, -0.09], 13);
+const map = L.map("map").setView([55.8, -4.3], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attritution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>`
-}).addTo(map);
-
-let circle = L.circle([51.508, -0.09], {
-    color: "red",
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
 }).addTo(map);
 
 fetch("/all_locations")
