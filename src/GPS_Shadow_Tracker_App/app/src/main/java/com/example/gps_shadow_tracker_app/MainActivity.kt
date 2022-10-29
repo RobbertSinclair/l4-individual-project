28e4d2cea5fb9ea0e2474940c23333d9cfbea3e8
robbert.sinclair <2432710S@student.gla.ac.uk>
2022-10-29 15:19:47 +0100

    Added the current map feature

--- src/GPS_Shadow_Tracker_App/app/src/main/java/com/example/gps_shadow_tracker_app/MainActivity.kt
+++ src/GPS_Shadow_Tracker_App/app/src/main/java/com/example/gps_shadow_tracker_app/MainActivity.kt
@@ -7,17 +7,32 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gps_shadow_tracker_app.gps.GPSService
 import com.example.gps_shadow_tracker_app.ui.LocationTextViews
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var locationText: LocationTextViews;
     private lateinit var gpsService: GPSService;
+    private lateinit var gpsMap: MapView;
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
         locationText = LocationTextViews(this);
+        //gpsMap = this.findViewById(R.id.gpsMap);
         gpsService = GPSService(this, locationText);
+        //val mapFragment: SupportMapFragment = getSupportFragmentManager().findFragmentById(R.id.gpsMap) as SupportMapFragment;
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        map.addMarker(MarkerOptions()
+            .position(LatLng(0 as Double,0 as Double))
+            .title("Marker"));
     }
 
 }
