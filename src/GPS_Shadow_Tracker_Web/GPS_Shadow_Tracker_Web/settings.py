1b8398de7fd27e01e3030980e8aabda35fb7b0ed
robbert.sinclair <2432710S@student.gla.ac.uk>
2022-11-04 11:42:07 +0000

    Added gunicorn

--- src/GPS_Shadow_Tracker_Web/GPS_Shadow_Tracker_Web/settings.py
+++ src/GPS_Shadow_Tracker_Web/GPS_Shadow_Tracker_Web/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["GPS_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "gpsshadows.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "gpsshadows.pythonanywhere.com", "l4-individual-project-production.up.railway.app"]
 
 
 # Application definition
